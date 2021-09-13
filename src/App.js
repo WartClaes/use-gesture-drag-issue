1a65b8a23bb44150be15307b48e3d90c9fb42110
Wart Claes <wart.claes@gmail.com>
2021-09-13 09:42:07 +0200

    feat(use-gesture): add use-gesture issue repro case

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import { useDrag } from 'react-use-gesture';
+import { useSpring, animated } from '@react-spring/web';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const bodyWidth = document.body.clientWidth;
+    const BLOCK_SIZE = 150;
+
+    const START_X = bodyWidth - BLOCK_SIZE;
+    const [{ x, y }, api] = useSpring(() => ({ x: START_X, y: 0 }));
+
+    // Set the drag hook and define component movement based on gesture data
+    const bind = useDrag(({ down, movement: [mx, my] }) => {
+        api.start({ x: down ? mx : START_X, y: down ? my : 0 });
+    });
+
+    // Bind it to a component
+    return <animated.div 
+        className="block"
+        style={{ 
+            x,
+            y,
+            '--size': `${BLOCK_SIZE}px`,
+        }}
+        {...bind()} 
+    />;
 }
 
 export default App;
